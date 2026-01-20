7fe4eb27c78534d3e11cd313e1f231f085418b02
narimanshariat <narimanshariat@users.noreply.github.com>
2026-01-20 11:24:00 -0800

    fix: Remove leftover merge conflict marker in Properties/Show.jsx

--- resources/js/pages/Properties/Show.jsx
+++ resources/js/pages/Properties/Show.jsx
@@ -181,7 +181,6 @@ export default function PropertyShow({
         <Layout>
             <Head title={property.name} />
 
-<<<<<<< HEAD
             {/* Sticky Property Header */}
             <PageHeader
                 title={property.name}
